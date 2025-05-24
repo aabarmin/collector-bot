fd48eba5a06c03bcc602a99b303b3c929fb8a473
Aleksandr Barmin <abarmin@outlook.com>
2025-05-24 16:11:49 +0100

    Fix issue with deployment

--- src/main/java/dev/abarmin/telegram/collector/CollectorBotApplication.java
+++ src/main/java/dev/abarmin/telegram/collector/CollectorBotApplication.java
@@ -2,15 +2,20 @@ package dev.abarmin.telegram.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CollectorBotConfiguration.class)
-public class CollectorBotApplication {
+public class CollectorBotApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollectorBotApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CollectorBotApplication.class);
+	}
 }
-
